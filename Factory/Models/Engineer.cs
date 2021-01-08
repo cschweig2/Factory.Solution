8438dbbebd5d27cc085f86cbbca20313978da605
Chelsea Becker <cschweig2@gmail.com>
2021-01-08 08:58:06 -0800

    add properties to Engineer class

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,8 +1,18 @@
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
     public class Engineer
     {
+        public class Engineer()
+        {
+            this.Machines = new HashSet<EngineerMachine>();
+        }
+        public int EngineerId { get; set; }
+        public string EngineerName { get; set; }
+        public string Certification { get; set; }
+        public ICollection<EngineerMachine> Machines { get; set; }
     }
 }
