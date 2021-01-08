bc246ae5b0944615dfc885afdfb1209986890fce
Chelsea Becker <cschweig2@gmail.com>
2021-01-08 11:58:43 -0800

    add details view

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -35,6 +35,15 @@ namespace Factory.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult Details(int id)
+        {
+            var thisMachine = _db.Machines
+                .Include(machine => machine.Engineers)
+                .ThenInclude(join => join.Engineer)
+                .FirstOrDefault(machine => machine.MachineId == id);
+            return View(thisMachine);
+        }
+
 
     }
 }
