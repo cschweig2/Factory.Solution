ebd3d3306ed60192808f20a7d2e39e433e8db4d6
Chelsea Becker <cschweig2@gmail.com>
2021-01-08 08:51:41 -0800

    add classes to context

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -4,7 +4,9 @@ namespace Factory.Models
 {
     public class FactoryContext : DbContext
     {
-        // public virtual DbSet<Object> Objects { get; set; }
+        public virtual DbSet<Engineer> Engineers { get; set; }
+        public virtual DbSet<Machine> Machines { get; set; }
+        public virtual DbSet<EngineerMachine> EngineerMachine { get; set; }
 
         public FactoryContext(DbContextOptions options) : base(options) { }
     }
